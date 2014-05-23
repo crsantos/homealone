08707e4c803827d41b9fef90c08cfd784c8c2134
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-23 19:42:45 +0100

    Renamed test files & added read value in order to toggle

--- tests/test_gpio_close.js
+++ tests/test_gpio_close.js
@@ -2,7 +2,7 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-23 19:36:23
+* @Last Modified time: 2014-05-23 19:42:05
 */
 
 var gpio = require("pi-gpio");
@@ -11,13 +11,19 @@ var pinNumber = process.argv[2]
 
 if (pinNumber) {
 
+  gpio.read(pinNumber, function(err, value) {
+    if(err) {
+      throw err;
+    }
+    console.log(value);    // The current state of the pin
+  });
+
   gpio.open(pinNumber, "output", function(err) {              // Open pin 3 for output
     console.log("Opened " + pinNumber);
-    gpio.write(pinNumber, 0, function() {                     // Set pin 3 high (1)
-      console.log("wrote zero to " + pinNumber);
+    gpio.write(pinNumber, 1, function() {                   // Set pin 3 high (1)
+      console.log("wrote one to " + pinNumber);
       gpio.close(pinNumber);
     });
   });
 
 };
-
