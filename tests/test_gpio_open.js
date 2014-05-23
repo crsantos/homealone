d599e80f0fbc0e1808d2c932533d135b10a104bb
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-23 19:47:27 +0100

    Toggling test

--- tests/test_gpio_open.js
+++ tests/test_gpio_open.js
@@ -2,7 +2,7 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-23 19:41:56
+* @Last Modified time: 2014-05-23 19:47:00
 */
 
 var gpio = require("pi-gpio");
@@ -11,19 +11,21 @@ var pinNumber = process.argv[2]
 
 if (pinNumber) {
 
-  gpio.read(pinNumber, function(err, value) {
-    if(err) {
-      throw err;
-    }
-    console.log(value);    // The current state of the pin
-  });
-
   gpio.open(pinNumber, "output", function(err) {              // Open pin 3 for output
     console.log("Opened " + pinNumber);
-    gpio.write(pinNumber, 0, function() {                     // Set pin 3 high (1)
-      console.log("wrote zero to " + pinNumber);
-      gpio.close(pinNumber);
-    });
+
+      gpio.read(pinNumber, function(err, value) {
+        if(err) {
+          throw err;
+        }
+        console.log("Read: "+ value);    // The current state of the pin
+
+        gpio.write(pinNumber, !value, function() {                     // Set pin 3 high (1)
+          console.log("wrote "+!value+" to " + pinNumber);
+          gpio.close(pinNumber);
+        });
+
+      });
   });
 
 };
