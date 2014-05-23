dd847b28f290b80bf87711fc64d4a7b9e54a9364
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-23 19:36:41 +0100

    On Off pin test update

--- tests/test_gpio3_open.js
+++ tests/test_gpio3_open.js
@@ -2,7 +2,7 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-23 19:36:15
+* @Last Modified time: 2014-05-23 19:36:23
 */
 
 var gpio = require("pi-gpio");
@@ -13,10 +13,11 @@ if (pinNumber) {
 
   gpio.open(pinNumber, "output", function(err) {              // Open pin 3 for output
     console.log("Opened " + pinNumber);
-    gpio.write(pinNumber, 1, function() {                   // Set pin 3 high (1)
-      console.log("wrote one to " + pinNumber);
+    gpio.write(pinNumber, 0, function() {                     // Set pin 3 high (1)
+      console.log("wrote zero to " + pinNumber);
       gpio.close(pinNumber);
     });
   });
 
 };
+
