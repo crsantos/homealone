c18432d24c86e84bc451c41f5553172f401b9ce7
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-24 00:31:40 +0100

    Validating test script params

--- tests/test_gpio_open.js
+++ tests/test_gpio_open.js
@@ -2,19 +2,21 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-23 19:49:34
+* @Last Modified time: 2014-05-24 00:30:58
 */
 
 var gpio = require("pi-gpio");
 
-var pinNumber = process.argv[2]
+if ( process.argv.length == 4 ) {
 
-if (pinNumber) {
+  var pinNumber = process.argv[2]
+  var state     = process.argv[3] == 1 ? 1 : 0
 
   gpio.open(pinNumber, "output", function(err) {              // Open pin 3 for output
     console.log("Opened " + pinNumber);
 
       gpio.read(pinNumber, function(err, value) {
+
         if(err) {
           throw err;
         }
@@ -28,5 +30,8 @@ if (pinNumber) {
       });
   });
 
-};
+} else {
+
+    console.log("Error. Usage: " + process.argv[0] + "filename $PIN_NUMBER [1|0]");
+}
 
