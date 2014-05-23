eced087b8ef6e03fdfd2c27f350e8e50f26cf2e3
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-23 19:28:41 +0100

    New open/close tests for pin3

--- tests/test_gpio3_close.js
+++ tests/test_gpio3_close.js
@@ -2,15 +2,18 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-23 19:28:04
+* @Last Modified time: 2014-05-23 19:28:24
 */
 
 var gpio = require("pi-gpio");
 
 gpio.open(3, "output", function(err) {              // Open pin 3 for output
+
   console.log("Opened 3");
-  gpio.write(3, 1, function() {                   // Set pin 3 high (1)
-    console.log("wrote one to 3 ");
+  gpio.write(3, 0, function() {                   // Set pin 3 high (1)
+    console.log("wrote zero to 3 ");
     gpio.close(3);
+
   });
+
 });
