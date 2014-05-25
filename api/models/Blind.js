06ffee2fa40a7186319bd71bb6b3d1bda5ec5959
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-25 10:02:57 +0100

    Updated comments

--- api/models/Blind.js
+++ api/models/Blind.js
@@ -1,7 +1,7 @@
 /**
  * Blind
  *
- * @module      :: Model
+ * @module      :: Blind
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,7 @@
 /**
  * User
  *
- * @module      :: Model
+ * @module      :: User
  * @description :: A short summary of how this model works and what it represents.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
