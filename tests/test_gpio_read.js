ded46d0feffc7d5255770d59cf2b7e5191f8d607
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-05-30 23:36:20 +0100

    Separated exception from success case

--- tests/test_gpio_read.js
+++ tests/test_gpio_read.js
@@ -2,7 +2,7 @@
 * @Author: crsantos
 * @Date:   2014-05-23 18:56:01
 * @Last Modified by:   crsantos
-* @Last Modified time: 2014-05-30 23:32:36
+* @Last Modified time: 2014-05-30 23:36:04
 */
 
 var gpio = require("pi-gpio");
@@ -16,9 +16,13 @@ if ( process.argv.length == 3 ) {
   gpio.read(pinNumber, function(err, value) {
 
     if(err) {
+
       console.log(err);
+
+    } else{
+
+      console.log("Read: "+ value); // The current state of the pin
     }
-    console.log("Read: "+ value); // The current state of the pin
 
     gpio.close(pinNumber);
 
